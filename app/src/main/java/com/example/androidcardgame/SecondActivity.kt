5833bd920e78c181eb711cf68b2d6eaed3c5c501
Viktor <vjagebrant@gmail.com>
2022-11-24 13:13:15 +0100

    textview add

--- app/src/main/java/com/example/androidcardgame/SecondActivity.kt
+++ app/src/main/java/com/example/androidcardgame/SecondActivity.kt
@@ -3,13 +3,19 @@ package com.example.androidcardgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    lateinit var coinText : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        coinText = findViewById(R.id.coinsTextView)
+        coinText.text = "Total Coins: 30"
 
 
 
