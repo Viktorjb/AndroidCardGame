ba40df8f313bf77c87a3a89903e3eee8f7f51744
Viktor <vjagebrant@gmail.com>
2022-11-24 13:20:34 +0100

    buttons and stuff

--- app/src/main/java/com/example/androidcardgame/SecondActivity.kt
+++ app/src/main/java/com/example/androidcardgame/SecondActivity.kt
@@ -3,11 +3,13 @@ package com.example.androidcardgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
 
     lateinit var coinText : TextView
+    lateinit var enterCoins : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -16,6 +18,11 @@ class SecondActivity : AppCompatActivity() {
 
         coinText = findViewById(R.id.coinsTextView)
         coinText.text = "Total Coins: 30"
+
+        enterCoins = findViewById(R.id.enterCoinsEditText)
+
+        var hibutton = findViewById<Button>(R.id.hiButton)
+        var lobutton = findViewById<Button>(R.id.loButton)
 
 
 
